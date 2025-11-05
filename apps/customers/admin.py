8336838c311db578de0d31735f3af66fcdaa2e40
Gabriel Alvin Aquino <userdeveloper554@gmail.com>
2025-11-05 07:33:56 +0100

    [test and fixed] customers templates

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Account, BillingCycle, Country, Currency
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -12,3 +12,27 @@ class CustomerAdmin(admin.ModelAdmin):
         if not change:  # If creating new object
             obj.created_by = request.user
         super().save_model(request, obj, form, change)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['name', 'customer', 'currency', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['name', 'customer__name']
+    readonly_fields = ['created_at', 'created_by']
+    
+    def save_model(self, request, obj, form, change):
+        if not change:  # If creating new object
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(BillingCycle)
+class BillingCycleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'cycle_type', 'created_at']
+    search_fields = ['name']
+    
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'is_active']
+    search_fields = ['name', 'code']
